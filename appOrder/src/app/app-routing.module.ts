f5d8c46997ae9e5b73cecd58b8c9d873638116b6
VanAnh <1910115@dlu.edu.vn>
2022-05-24 09:31:31 +0700

    .

--- appOrder/src/app/app-routing.module.ts
+++ appOrder/src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home', 
+    redirectTo: 'setting', 
     pathMatch: 'full'
   },
   {
@@ -41,18 +41,6 @@ const routes: Routes = [
     path: 'setting',
     loadChildren: () => import('./setting/setting.module').then( m => m.SettingPageModule)
   },
-  {
-  path: 'message',
-    loadChildren: () => import('./message/message.module').then( m => m.MessagePageModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
-  },
-  {
-    path: 'my-orders',
-    loadChildren: () => import('./my-orders/my-orders.module').then( m => m.MyOrdersPageModule)
-  },
 
 
 ];
@@ -64,3 +52,72 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+// import { NgModule } from '@angular/core';
+// import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [
+//   {
+//     path: 'home',
+//     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+//   },
+
+//   {
+//     path: 'info-food',
+//     loadChildren: () => import('./info-food/info-food.module').then( m => m.InfoFoodPageModule)
+//   },
+//   {
+//     path: 'order-food',
+//     loadChildren: () => import('./order-food/order-food.module').then( m => m.OrderFoodPageModule)
+//   },
+
+//   {
+//     path: 'login',
+//     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+//   },
+//   {
+//     path: 'register',
+//     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+//   },
+//   {
+//     path: '',
+//     redirectTo: 'home', 
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: 'rating',
+//     loadChildren: () => import('./rating/rating.module').then( m => m.RatingPageModule)
+//   },
+//   {
+//     path: 'profile',
+//     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+//   },
+//   {
+//     path: 'setting',
+//     loadChildren: () => import('./setting/setting.module').then( m => m.SettingPageModule)
+//   },
+//   {
+//   path: 'message',
+//     loadChildren: () => import('./message/message.module').then( m => m.MessagePageModule)
+//   },
+//   {
+//     path: 'contact',
+//     loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+//   },
+//   {
+//     path: 'my-orders',
+//     loadChildren: () => import('./my-orders/my-orders.module').then( m => m.MyOrdersPageModule)
+//   },
+
+
+// ];
+
+// @NgModule({
+//   imports: [
+//     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+//   ],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
