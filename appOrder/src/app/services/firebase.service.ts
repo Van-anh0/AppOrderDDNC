e22418f0cbfcecb04dc0562292e41706c5cf31ea
VanAnh <1910115@dlu.edu.vn>
2022-05-27 15:48:54 +0700

    cham

--- appOrder/src/app/services/firebase.service.ts
+++ appOrder/src/app/services/firebase.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Auth, createUserWithEmailAndPassword} from '@angular/fire/auth'
+// import {Auth, createUserWithEmailAndPassword} from '@angular/fire/auth'
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
   constructor(
-    private auth: Auth
+    // private auth: Auth
   ) {}
 
-  async register({email, password}){
-    const user = await createUserWithEmailAndPassword(this.auth, email, password);
-    return user;
-  }
+  // async register({email, password}){
+  //   const user = await createUserWithEmailAndPassword(this.auth, email, password);
+  //   return user;
+  // }
 
 }
