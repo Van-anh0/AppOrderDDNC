2ee7bfed24211e7ecb3c183ec6240bd2cfce6c8a
TsunCoder <thiensoncva01@gmail.com>
2022-06-05 22:01:32 +0700

    Food, user, table

--- server/DataImport.js
+++ server/DataImport.js
@@ -3,7 +3,10 @@ const user = require('./model/user.js');
 const users = require('./Data/users.js');
 const category = require('./model/category.js');
 const categories = require('./Data/category.js');
-
+const table = require('./model/table.js');
+const tables = require('./Data/table.js');
+const food = require('./model/food.js');
+const products = require('./Data/products.js');
 const asyncHandler = require('express-async-handler');
 
 const ImportData = express.Router();
@@ -23,6 +26,24 @@ ImportData.post(
         await category.remove({});
         const importCategories = await category.insertMany(categories);
         res.send({importCategories});
+    })
+)
+
+ImportData.post(
+    "/table",
+    asyncHandler(async(req, res) => {
+        await table.remove({});
+        const importTable = await table.insertMany(tables);
+        res.send({importTable});
+    })
+)
+
+ImportData.post(
+    "/products",
+    asyncHandler(async(req, res) => {
+        await food.remove({});
+        const importProducts = await food.insertMany(products);
+        res.send({importProducts});
     })
 )
 
--- server/model/food.js
+++ server/model/food.js
@@ -1,30 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const foodSchema = mongoose.Schema(
-    {
-        title:{
-            type: String,
-            required: true,
-        },
-        description:{
-            type: String,
-        },
-        category: {
-            type: mongoose.Types.ObjectId,
-            ref: 'Category'
-        }, 
-        rating:{
-            rate:{
-                type: Number,
-                min: 0,
-                max: 5,
-                default: 0
-            },
-            count: {
-                type: Number,
-                default: 0
-            }
-        } 
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {timestamps: true}
-)
-module.exports = mongoose.model('Food', foodSchema)
+    description: {
+      type: String,
+    },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "Category",
+    },
+    price: {
+      type: Number,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    rating: {
+      rate: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0,
+      },
+      count: {
+        type: Number,
+        default: 0,
+      },
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Food", foodSchema);
