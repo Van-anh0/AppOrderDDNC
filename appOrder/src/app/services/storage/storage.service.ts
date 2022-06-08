6317c4a25064ec0d5490a1273e403bf58bb8891e
TsunCoder <thiensoncva01@gmail.com>
2022-06-09 00:42:42 +0700

    save localstorage

--- appOrder/src/app/services/storage/storage.service.ts
+++ appOrder/src/app/services/storage/storage.service.ts
@@ -3,37 +3,33 @@ import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { escape, unescape } from 'querystring';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
-
   constructor(private storage: Storage) {}
-  
-  async store(storageKey: string, value: any){
+
+  async store(storageKey: string, value: any) {
     const encryptedValue = btoa(escape(JSON.stringify(value)));
-    return await this.storage.set(storageKey,encryptedValue);
+    return await this.storage.set(storageKey, encryptedValue);
   }
 
-  async get (storageKey: string){
-    return new Promise(resolve => {
-      this.storage.get(storageKey).then((value)=> {
-        if(value == null){
+  async get(storageKey: string) {
+    return new Promise((resolve) => {
+      this.storage.get(storageKey).then((value) => {
+        if (value == null) {
           resolve(false);
-        }
-        else{
+        } else {
           resolve(JSON.parse(unescape(atob(value))));
         }
       });
     });
   }
 
-  async removeItem (strorageKey: string){
+  async removeItem(strorageKey: string) {
     await this.storage.remove(strorageKey);
   }
 
-  async clean(){
+  async clean() {
     await this.storage.clear();
   }
 }
-
-
